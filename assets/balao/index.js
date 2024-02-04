73041043f12ab79fb6e0612ec4b586fbedfd115c
Paulo Roberto <34938982+paulorobertofernandes@users.noreply.github.com>
2024-02-04 10:12:30 -0300

    Add files via upload

---
+++ assets/balao/index.js
@@ -0,0 +1,10 @@
+<!-- Adicione o script JavaScript no final para garantir que o DOM esteja carregado -->
+    <script>
+        // Função para exibir o balão
+        function showBalloon() {
+            document.getElementById('floating-balloon').style.display = 'block';
+        }
+
+        // Função para fechar o balão
+        function closeBalloon() {
+            documen
